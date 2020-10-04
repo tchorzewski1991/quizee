aebc8a56ef672af855a7e66ac23625b0c1193d47
tchorzewski1991 <rafal.tchorzewski@airhelp.com>
2020-10-04 23:07:49 +0200

    Make a question text color more visible

--- lib/main.dart
+++ lib/main.dart
@@ -58,7 +58,7 @@ class _QuizPageState extends State<QuizPage> {
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
-                  color: Colors.black,
+                  color: Colors.white,
                 ),
               ),
             ),
